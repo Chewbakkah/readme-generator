a142c16cadc833a2b7831d154ad073e5c5d28ca0
Chewbakkah <chewbakkah@gmail.com>
2022-01-15 01:26:26 -0600

    new test

--- saved-readmes/README-asdf.md
+++ saved-readmes/README-asdf.md
@@ -1,21 +1,32 @@
 # asdf
-### Created by: Chewbakkah | [Github](https://github.com/Chewbakkah) | Email: asdf
+### Created by: asdf | [Github](https://github.com/asdf) | Email: asdf
 ## Description
 asdf
-
-## Project Installation Instructions
+## Table Of Contents
+* [Installation Instructions](#Project-Installation-Instructions)
+* [Installation Instructions](#Project-Usage-Instructions)
+* [Installation Instructions](#Project-Collaborators)
+* [Installation Instructions](#Project-Features)
+* [Installation Instructions](#Contribution-Guidelines)
+* [Installation Instructions](#Project-Testing-Instructions)
+* [Installation Instructions](#Project-Snapshot)
+* [Installation Instructions](#License)
+## Project-Installation-Instructions
   asdf
-## Project Usage Instructions
+## Project-Usage-Instructions
   asdf
-
-## Project Features
+## Project-Collaborators
+    [chewbakkah](https://github.com/chewbakkah)    
+## Project-Features
   asdf
-
-## Project Testing Instructions
+## Contribution-Guidelines
+  asdf
+## Project-Testing-Instructions
   adsf
-## Project Snapshot
-![Project Snapshot](https://github.com/Chewbakkah/readme-generator/blob/dev/snapshot/snapshot.JPG)
+## Project-Snapshot
+![Project Snapshot](asdf)
 ## License
   This project is licensed under
 ![License](https://img.shields.io/badge/License-MIT-blue.svg)
+     
 [More Info](https://choosealicense.com/licenses/)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,7 +11,8 @@ function renderLicenseText(projectLicense) {
 }
 function renderLicenseBadge(projectLicense) {
   if (projectLicense !== "None") {
-    return `![License](https://img.shields.io/badge/License-${projectLicense}-blue.svg)`;
+    return `![License](https://img.shields.io/badge/License-${projectLicense}-blue.svg)
+     `;
   } else {
     return "";
   }
@@ -25,104 +26,143 @@ function renderLicenseLink(projectLicense) {
   }
 }
 
-function renderInstallation(projectInstallation){
-  if (projectInstallation !== '') {
-    return `## Project Installation Instructions
-  ${projectInstallation}`
-   ;
+function renderInstallation(projectInstallation) {
+  if (projectInstallation !== "") {
+    return `## Project-Installation-Instructions
+  ${projectInstallation}`;
   } else {
     return "";
   }
 }
 
-function renderUsage(projectUsage){
-  if (projectUsage !== '') {
-    return `## Project Usage Instructions
-  ${projectUsage}`
-   ;
+function renderUsage(projectUsage) {
+  if (projectUsage !== "") {
+    return `## Project-Usage-Instructions
+  ${projectUsage}`;
   } else {
     return "";
   }
 }
 
-function renderCollabList(projectCollabVerify, projectCollabList){
+function renderCollabList(projectCollabVerify, projectCollabList) {
   if (projectCollabVerify == true) {
-    let collabStr = '';
-    let collab = projectCollabList.split(',').map(function(item){
+    let collabStr = "";
+    let collab = projectCollabList.split(",").map(function (item) {
       return item.trim();
     });
-    for(i=0; i<collab.length; i++){
-      collabStr += "[" + collab[i] + "](https://github.com/" + collab[i] + ")    "
+    for (i = 0; i < collab.length; i++) {
+      collabStr +=
+        "[" + collab[i] + "](https://github.com/" + collab[i] + ")    ";
     }
-    return `## Project Collaborators
-    ${collabStr}`
-     ;
+    return `## Project-Collaborators
+    ${collabStr}`;
   } else {
     return "";
   }
 }
 
-function renderFeatures(projectFeatures){
-  if (projectFeatures !== '') {
-    return `## Project Features
-  ${projectFeatures}`
-     ;
+function renderFeatures(projectFeatures) {
+  if (projectFeatures !== "") {
+    return `## Project-Features
+  ${projectFeatures}`;
   } else {
     return "";
   }
 }
 
-function renderContrib(projectContribVerify, projectContribInstruct){
+function renderContrib(projectContribVerify, projectContribInstruct) {
   if (projectContribVerify == true) {
-    return `## Contribution Guidelines
-  ${projectContribInstruct}`
-     ;
+    return `## Contribution-Guidelines
+  ${projectContribInstruct}`;
   } else {
     return "";
   }
 }
 
-function renderTest(projectTest){
-  if (projectTest !== '') {
-    return `## Project Testing Instructions
-  ${projectTest}`
-     ;
+function renderTest(projectTest) {
+  if (projectTest !== "") {
+    return `## Project-Testing-Instructions
+  ${projectTest}`;
   } else {
     return "";
   }
 }
 
-function renderIMG(projectIMG){
-  if (projectIMG !== '') {
-    return `## Project Snapshot
-![Project Snapshot](${projectIMG})`
-     ;
+function renderIMG(projectIMG) {
+  if (projectIMG !== "") {
+    return `## Project-Snapshot
+![Project Snapshot](${projectIMG})`;
   } else {
     return "";
   }
 }
 
-
+function renderTableOfContents(
+  projectInstallation,
+  projectUsage,
+  projectCollabVerify,
+  projectFeatures,
+  projectContribVerify,
+  projectTest,
+  projectIMG,
+  projectLicense
+) {
+  let TOC = '';
+  if (projectInstallation !== "") {
+    TOC += "* [Installation Instructions](#Project-Installation-Instructions)\n";
+  }
+  if (projectUsage !== "") {
+    TOC += "* [Installation Instructions](#Project-Usage-Instructions)\n";
+  }
+  if (projectCollabVerify == true) {
+    TOC += "* [Installation Instructions](#Project-Collaborators)\n";
+  }
+  if (projectFeatures !== "") {
+    TOC += "* [Installation Instructions](#Project-Features)\n";
+  }
+  if (projectContribVerify == true) {
+    TOC += "* [Installation Instructions](#Contribution-Guidelines)\n";
+  }
+  if (projectTest !== "") {
+    TOC += "* [Installation Instructions](#Project-Testing-Instructions)\n";
+  }
+  if (projectIMG !== "") {
+    TOC += "* [Installation Instructions](#Project-Snapshot)\n";
+  }
+  if (projectLicense !== "") {
+    TOC += "* [Installation Instructions](#License)";
+  }
+  return `## Table Of Contents
+${TOC}`;
+}
 // ## Table of Contents
 // * [Installation](#installation)
 // * [Usage](#installation)
 // * [Credits](#installation)
 
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.projectTitle}
-### Created by: ${data.name} | [Github](https://github.com/${data.name}) | Email: ${
-    data.email
-  }
+### Created by: ${data.name} | [Github](https://github.com/${
+    data.name
+  }) | Email: ${data.email}
 ## Description
 ${data.projectDescription}
-
+${renderTableOfContents(
+  data.projectInstallation,
+  data.projectUsage,
+  data.projectCollabVerify,
+  data.projectFeatures,
+  data.projectContribVerify,
+  data.projectTest,
+  data.projectIMG,
+  data.projectLicense
+)}
 ${renderInstallation(data.projectInstallation)}
 ${renderUsage(data.projectUsage)}
-${renderCollabList(data.projectCollabList)}
+${renderCollabList(data.projectCollabVerify, data.projectCollabList)}
 ${renderFeatures(data.projectFeatures)}
-${renderContrib(data.projectContribInstruct)}
+${renderContrib(data.projectContribVerify, data.projectContribInstruct)}
 ${renderTest(data.projectTest)}
 ${renderIMG(data.projectIMG)}
 ${renderLicenseText(data.projectLicense)}
